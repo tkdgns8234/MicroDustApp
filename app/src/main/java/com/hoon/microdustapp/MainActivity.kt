d667a2a140a88ac4cece51a62fef09a9859206db
tkdgns8234 <55433939+tkdgns8234@users.noreply.github.com>
2022-08-18 01:06:36 +0900

    미세먼지 앱 V1.3.0

--- app/src/main/java/com/hoon/microdustapp/MainActivity.kt
+++ app/src/main/java/com/hoon/microdustapp/MainActivity.kt
@@ -21,6 +21,7 @@ import com.hoon.microdustapp.data.api.RetrofitInstance.getMeasureInfo
 import com.hoon.microdustapp.data.api.RetrofitInstance.getNearbyMeasuringStation
 import com.hoon.microdustapp.data.model.AirPollutionModel
 import com.hoon.microdustapp.data.model.measure.MeasureResult
+import com.hoon.microdustapp.data.util.constants.AirPollution
 import com.hoon.microdustapp.databinding.ActivityMainBinding
 import com.hoon.microdustapp.ui.adapter.AirPollutionListAdapter
 import kotlinx.coroutines.*
@@ -36,11 +37,18 @@ import java.util.*
 코루틴 수명주기 맞추기 아래 글 참조
 https://origogi.github.io/coroutine/%EC%BD%94%EB%A3%A8%ED%8B%B4-%EC%8A%A4%EC%BD%94%ED%94%84/
 
+progress
+// max값을 먼저 지정해야함 (sdk 코드 보면 progress 적용 시 값을 max 이상 min 이하로 지정할 수 없음)
+
+
 측정소 지도 구현(simple 8도 지도)
 
 예보, 예보시간, 미세먼지 gif 이미지
 http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?serviceKey=cgQ0LFIb5z3rpBZC9JvmId2VpJGKSAY3OSgLG%2B6ueLWpYiJZbpYBQ%2FqWPCUbjgegEqaSU1yCO5JseVLBu21LnQ%3D%3D&returnType=json&numOfRows=700&searchDate=2022-08-16&ver=1.1
 
+뷰페이저 + 인디케이터 만들기
+
+8도 미세먼지 지도 만들기 -> ui 어떻게 위치시킬지 생각해보기
 
 행동요령 -> 거의 null만 들어옴
 
@@ -149,13 +157,63 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateRecyclerView(measureResult: MeasureResult) {
         val models = mutableListOf<AirPollutionModel>()
-        models.add(AirPollutionModel("초미세먼지", measureResult.pm25Grade, measureResult.pm25Value))
-        models.add(AirPollutionModel("미세먼지", measureResult.pm10Grade, measureResult.pm10Value))
-        models.add(AirPollutionModel("통합환경지수", measureResult.khaiGrade, measureResult.khaiValue))
-        models.add(AirPollutionModel("오존", measureResult.o3Grade, measureResult.o3Value))
-        models.add(AirPollutionModel("이산화질소", measureResult.no2Grade, measureResult.no2Value))
-        models.add(AirPollutionModel("일산화탄소", measureResult.coGrade, measureResult.coValue))
-        models.add(AirPollutionModel("아황산가스", measureResult.so2Grade, measureResult.so2Value))
+
+        models.add(
+            AirPollutionModel(
+                AirPollution.PM_10.pollutionName,
+                measureResult.pm10Grade,
+                measureResult.pm10Value,
+                AirPollution.PM_10.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.PM_2_5.pollutionName,
+                measureResult.pm25Grade,
+                measureResult.pm25Value,
+                AirPollution.PM_2_5.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.CAI.pollutionName,
+                measureResult.khaiGrade,
+                measureResult.khaiValue,
+                AirPollution.CAI.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.O3.pollutionName,
+                measureResult.o3Grade,
+                measureResult.o3Value,
+                AirPollution.O3.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.NO2.pollutionName,
+                measureResult.no2Grade,
+                measureResult.no2Value,
+                AirPollution.NO2.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.CO.pollutionName,
+                measureResult.coGrade,
+                measureResult.coValue,
+                AirPollution.CO.maximumValue
+            )
+        )
+        models.add(
+            AirPollutionModel(
+                AirPollution.SO2.pollutionName,
+                measureResult.so2Grade,
+                measureResult.so2Value,
+                AirPollution.SO2.maximumValue
+            )
+        )
 
         recyclerViewAdapter.submitList(models)
     }
@@ -168,18 +226,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateGradeInfo(measureResult: MeasureResult) = with(binding) {
-        val totalGrade = measureResult.khaiGrade
+        val currentGrade = measureResult.pm10Grade
+        val currentVal = measureResult.pm10Value
 
-        ivEmoji.setImageResource(totalGrade.emojiDrawableID)
-        tvGrade.text = totalGrade.gradeText
-        tvDescription.text = resources.getString(totalGrade.descStringID)
-        layoutMainBackground.setBackgroundResource(totalGrade.colorID)
+        ivEmoji.setImageResource(currentGrade.emojiDrawableID)
+        tvGrade.text = currentGrade.gradeText
+        tvDescription.text = resources.getString(currentGrade.descStringID)
+        layoutMainBackground.setBackgroundResource(currentGrade.colorID)
 
-        microDustProgressbarText.text = "${measureResult.pm10Value}/150~"
-        microDustProgressbar.progress = measureResult.pm10Value.toIntOrNull() ?: 0
+        microDustProgressbarText.text = "${currentVal}/150~"
+        microDustProgressbar.progress = currentVal.toIntOrNull() ?: 0
         microDustProgressbar.progressTintList =
-            ColorStateList.valueOf(ContextCompat.getColor(this@MainActivity, totalGrade.colorID))
-
+            ColorStateList.valueOf(ContextCompat.getColor(this@MainActivity, currentGrade.colorID))
     }
 
     private fun updateAddressFromGps(latitude: Double, longitude: Double) {
--- app/src/main/java/com/hoon/microdustapp/data/model/AirPollutionModel.kt
+++ app/src/main/java/com/hoon/microdustapp/data/model/AirPollutionModel.kt
@@ -3,7 +3,8 @@ package com.hoon.microdustapp.data.model
 import com.hoon.microdustapp.data.model.measure.Grade
 
 data class AirPollutionModel(
-    val title: String,
-    val grade: Grade,
-    val value: String
+    val name: String, // 이름 e.g) CO2
+    val grade: Grade, // 등급 (좋음, 양호, 나쁨, 매우나쁨)
+    val value: String, // 수치
+    val maxValue: Double, // 최대 수치
 )
