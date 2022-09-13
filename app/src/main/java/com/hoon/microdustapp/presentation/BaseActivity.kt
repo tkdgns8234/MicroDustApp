691f7475b7cefe7ff7ab2aa6b4c9908d997335ca
tkdgns8234 <55433939+tkdgns8234@users.noreply.github.com>
2022-09-13 23:55:56 +0900

    미세먼지 앱 V3.1.1

--- app/src/main/java/com/hoon/microdustapp/presentation/BaseActivity.kt
+++ app/src/main/java/com/hoon/microdustapp/presentation/BaseActivity.kt
@@ -1,7 +1,6 @@
 package com.hoon.microdustapp.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.hoon.microdustapp.R
 import org.koin.androidx.scope.ScopeActivity
 
